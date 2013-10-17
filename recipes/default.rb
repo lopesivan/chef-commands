5c32b1e9bb9a751b1ce47040129301151ddef041
Ivan Lopes <ivanlopes@42algoritmos.com.br>
2013-10-16 23:55:51 -0300

    atualizando chef manager.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,67 @@
+template "#{ENV['HOME']}/commands.txt" do
+  source 'commands.txt.erb'
+  mode '0664'
+end
+
 #
-# Cookbook Name:: commands
-# Recipe:: default
+# Create a user.
+# manage_home is set to true so that his home directory will be created.
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+user "silva" do
+    comment "Silva is a User"
+    home "/home/silva"
+    shell "/bin/bash"
+    supports  :manage_home => true
+end
+ 
 #
-# All rights reserved - Do Not Redistribute
+# When executing a script, it should create a file specified by 
+# "creates" upon completion. This ensures that the command will 
+# only run once throughout the life of the system.
 #
+execute "a sample command" do
+    command "touch /home/silva/sample.txt"
+    creates "/home/silva/sample.txt"
+end
+ 
+#
+# Create a directory with specified ownership and permissions.
+#
+directory "/home/silva/silva-app" do
+    owner "silva"
+    group "silva"
+    mode 0755
+    #
+    # This weird syntax looks scary, but it's quite harmless. :-)
+    # In Ruby, if you have a string preceeded by a colon, it is 
+    # similar to having a string with single quotes around it.  
+    # There is a slight additional difference, but that is beyond 
+    # the scope of this blog post about Chef.
+    #
+    action :create
+end
+ 
+ 
+#
+# Create a configuration file based on a template.
+# This will ONLY run if the date of the template file is newer than the date 
+# of the deployed file. Chef is fairly efficient about things like that.  
+# It never does more work than is necessary.
+#
+template "/home/silva/silva-app/config.json" do
+    source "config.json.erb"
+    variables(
+        :home_dir => "/home/silva/silva-app"
+    )
+    user "silva"
+    group "silva"
+    mode 0600
+end
+ 
+#
+# Install the "toilet" package. Because every system needs a toilet.
+#
+package "toilet" do
+    action :install
+end
+
